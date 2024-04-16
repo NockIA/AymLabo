7ac0b528358787a72f13a2fb9eba112ae21aef8e
ckevin <ISeeNock.Draw@gmail.com>
2024-04-16 19:57:44 +0200

    :tada: | init with electron forge

---
+++ App-Electron/src/config/ways.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../pages/home";
+import ProfilePage from "../pages/profile";
+
+function Ways() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+        <Route path="*" element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Ways;
---
+++ App-Electron/src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function ProfilePage() {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+export default ProfilePage;
