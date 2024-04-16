13688b367d284f640853e5d0e5220b8ea3c5ac11
Ylucasb <lucasbigot@ynov.com>
2024-04-16 19:21:44 +0200

    :rocket: | finish api init

--- Api/Handlers/postHandler.go
+++ Api/Handlers/postHandler.go
@@ -1,12 +1,18 @@
 package handlers
 
-import "net/http"
+import (
+	methods "api/Handlers/Methods"
+	"net/http"
+)
 
 var handlers = map[string]func(http.ResponseWriter, *http.Request){
-	"/createGame": CreateGameHandler,
-	
+	"/register": methods.Register,
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-
+	if handler, ok := handlers[r.URL.Path]; ok {
+		handler(w, r)
+	} else {
+		http.Error(w, "Not Found", http.StatusNotFound)
+	}
 }
