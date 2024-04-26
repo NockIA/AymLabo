58ef8f43c0d0525e8e0faee32d5f6e3dfd239e9f
Ylucasb <lucasbigot@ynov.com>
2024-04-26 09:27:27 +0200

    :sparkles: | edit profile

--- Api/Handlers/putHandler.go
+++ Api/Handlers/putHandler.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	put "api/Handlers/Methods/Put"
 	"net/http"
 )
 
-var puthandlers = map[string]func(http.ResponseWriter, *http.Request){}
+var puthandlers = map[string]func(http.ResponseWriter, *http.Request){
+	"/profile": put.Profile,
+}
 
 func PutHandler(w http.ResponseWriter, r *http.Request) {
 	if handler, handlerExist := puthandlers[r.URL.Path]; handlerExist {
